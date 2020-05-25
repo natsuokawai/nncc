9bd323542e5016ffd65bab58346d57906f75ced5
natsuokawai <kawai.natsuo@gmail.com>
2020-05-25 09:24:35 +0900

    [wip] トークナイザを追加

--- main.c
+++ main.c
@@ -11,6 +11,25 @@ static void error(char *fmt, ...) {
   exit(1);
 }
 
+// トークンの種類
+typedef enum {
+  TK_RESERVED, // 予約後
+  TK_NUM,      // 数値
+  TK_EOF       // End-of-file
+} TokenKind;
+
+typedef struct Token {
+  TokenKind kind; // トークンの種類
+  Token *next;    // 次のトークン
+  long val;       // 数値（TK_NUM のとき）
+  char *str;      // 文字列（TK_RESERVED のとき）
+  int len;        // トークンの長さ
+} Token;
+
+static Token *tokenize(char *p) {
+  
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("%s: invalid number of arguments\n", argv[0]);
